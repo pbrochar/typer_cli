deb5e422bd209c48e6af39846a520380382a98c5
Paul <paul@debian.paul>
2021-09-14 22:07:09 +0200

    typer add_completion=false

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from sort_string import sort_string
 from invert_first_letters import invert_first_letters
 from define_words import define_words_in_sentence
 
-app = typer.Typer(help="Sort, invert letters or define words in a string")
+app = typer.Typer(add_completion=False, help="Sort, invert letters or define words in a string")
 
 
-@app.command(help="Sort the words in the string.")
+@app.command("sort", help="Sort the words in the string.")
 def sort(
         reverse: bool = typer.Option(False, "--desc", "-d", help="descending order"),
         insensitive: bool = typer.Option(False, "--insensitive", "-i", help="insensitive case sort"),
@@ -16,13 +16,13 @@ def sort(
     sort_string(reverse, insensitive, sentence)
 
 
-@app.command(help="Invert the first two letters of each word in the string.")
+@app.command("invert", help="Invert the first two letters of each word in the string.")
 def invert_letter(sentence: str = typer.Argument(...)) -> None:
     """Initializes the CLI 'invert-letter' command and calls the invert_first_letters function"""
     invert_first_letters(sentence)
 
 
-@app.command(help="Define, if possible, each word in the sentence.")
+@app.command("define", help="Define, if possible, each word in the sentence.")
 def define_words(sentence: str = typer.Argument(...)) -> None:
     """Initializes the CLI 'define-words' command and calls the define_words_in_sentence function"""
     define_words_in_sentence(sentence)

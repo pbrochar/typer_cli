fbd015beab49e8572bb20ff061b043a2e2bdd052
Paul <paul@debian.paul>
2021-09-15 13:19:16 +0200

    fix reverse letters

--- invert_first_letters.py
+++ invert_first_letters.py
@@ -1,5 +1,8 @@
 def invert_first_letters(sentence: str) -> None:
     """Invert the two first letters of each words and print it"""
     words = sentence.split()
-    for i in words:
-        print(i[1::-1] + i[2::])
+    for word in words:
+        print("".join([
+            word[i:i + 2][::-1]
+            for i in range(0, len(word), 2)
+        ]))
